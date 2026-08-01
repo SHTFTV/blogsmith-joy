b47d64d294faf9e7db3cd98556651ebf4542813b
Industry Army Marketing <frameinsteel@gmail.com>
2026-08-01 14:44:23 -0700

    deploy: push weddings.io codebase from zip [192/428]

--- src/routes/cities.tsx
+++ src/routes/cities.tsx
@@ -1,7 +1,5 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/cities")({
-  beforeLoad: () => {
-    throw redirect({ href: "/cities/index.html" });
-  },
+  component: () => <Outlet />,
 });
